7b0aa32b3ab0df51b31a1f04658a51b3b9ed447c
shiv13b <shivanh@beverensoftware.com>
2024-07-31 10:18:28 +0300

    changes in po.js

--- amcc_app/public/js/purchase_order.js
+++ amcc_app/public/js/purchase_order.js
@@ -1,32 +1,31 @@
 frappe.ui.form.on("Purchase Order", "onload", function(frm) {    
-    if(frm.doc.workflow_state === 'Pending For Manager' && frappe.user.has_role(['PO Manager'])){
-        frm.doc.po_manager_name = frappe.session.user
-  	frm.doc.pom_date = frappe.datetime.nowdate()
-      } 
-        else if(frm.doc.workflow_state === 'Pending For FC Approval' && frappe.user.has_role(['FC'])){
-        frm.doc.fc_name = frappe.session.user
-	frm.doc.fcm_date = frappe.datetime.nowdate()
-      }
-      
-      else if(frm.doc.workflow_state === 'Pending For GM Approval' && frappe.user.has_role(['GM'])){
-        frm.doc.gm_approver_name = frappe.session.user
-	frm.doc.gmm_date = frappe.datetime.nowdate()
-      }    
+  if(frm.doc.workflow_state === 'Pending For FC Approval' && frappe.user.has_role(['PO Manager'])){
+      frm.doc.po_manager_name = frappe.session.user
+  frm.doc.pom_date = frappe.datetime.nowdate()
+    } 
+      else if(frm.doc.workflow_state === 'Pending For GM Approval' && frappe.user.has_role(['FC'])){
+      frm.doc.fc_name = frappe.session.user
+frm.doc.fcm_date = frappe.datetime.nowdate()
+    }
+    
+    else if(frm.doc.workflow_state === 'Approved By GM' && frappe.user.has_role(['GM'])){
+      frm.doc.gm_approver_name = frappe.session.user
+frm.doc.gmm_date = frappe.datetime.nowdate()
+    }    
 });
 
 frappe.ui.form.on("Purchase Order", "validate", function(frm) {    
-    if(frm.doc.workflow_state === 'Pending For Manager' && frappe.user.has_role(['PO Manager'])){
-        frm.doc.po_manager_name = frappe.session.user
-	frm.doc.pom_date = frappe.datetime.nowdate()
-  
-      } 
-        else if(frm.doc.workflow_state === 'Pending For FC Approval' && frappe.user.has_role(['FC'])){
-        frm.doc.fc_name = frappe.session.user
-	frm.doc.fcm_date = frappe.datetime.nowdate()
-      }
-      
-      else if(frm.doc.workflow_state === 'Pending For GM Approval' && frappe.user.has_role(['GM'])){
-        frm.doc.gm_approver_name = frappe.session.user
-	frm.doc.gmm_date = frappe.datetime.nowdate()
-      }
+      if(frm.doc.workflow_state === 'Pending For FC Approval' && frappe.user.has_role(['PO Manager'])){
+      frm.doc.po_manager_name = frappe.session.user
+  frm.doc.pom_date = frappe.datetime.nowdate()
+    } 
+      else if(frm.doc.workflow_state === 'Pending For GM Approval' && frappe.user.has_role(['FC'])){
+      frm.doc.fc_name = frappe.session.user
+frm.doc.fcm_date = frappe.datetime.nowdate()
+    }
+    
+    else if(frm.doc.workflow_state === 'Approved By GM' && frappe.user.has_role(['GM'])){
+      frm.doc.gm_approver_name = frappe.session.user
+frm.doc.gmm_date = frappe.datetime.nowdate()
+    }
 });
