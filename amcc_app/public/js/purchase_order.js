cee579af3f4d74d17719a3c32e1ebac2310f65d6
shiv13b <shivanh@beverensoftware.com>
2024-08-03 11:25:10 +0300

    Changed

--- amcc_app/public/js/purchase_order.js
+++ amcc_app/public/js/purchase_order.js
@@ -1,31 +1,29 @@
-frappe.ui.form.on("Purchase Order", "onload", function(frm) {    
-  if(frm.doc.workflow_state === 'Pending For FC Approval' && frappe.user.has_role(['PO Manager'])){
-      frm.doc.po_manager_name = frappe.session.user
-  frm.doc.pom_date = frappe.datetime.nowdate()
-    } 
-      else if(frm.doc.workflow_state === 'Pending For GM Approval' && frappe.user.has_role(['FC'])){
-      frm.doc.fc_name = frappe.session.user
-frm.doc.fcm_date = frappe.datetime.nowdate()
-    }
-    
-    else if(frm.doc.workflow_state === 'Approved By GM' && frappe.user.has_role(['GM'])){
-      frm.doc.gm_approver_name = frappe.session.user
-frm.doc.gmm_date = frappe.datetime.nowdate()
-    }    
+frappe.ui.form.on("Purchase Order", {
+  before_workflow_action: (frm) => {
+      console.log(frm.selected_workflow_action);
+      if (frm.doc.workflow_state === "Pending For Manager") {
+          frm.set_value("po_manager_name", frappe.session.user);
+          frm.set_value("pom_date", frappe.datetime.nowdate());
+
+      }
+      else if(frm.doc.workflow_state === "Pending For FC Approval"){
+          
+              frm.set_value("fc_name", frappe.session.user);
+              frm.set_value("fcm_date", frappe.datetime.nowdate());
+
+
+      }
+      else if(frm.doc.workflow_state === "Pending For GM Approval"){
+              frm.set_value("gm_approver_name", frappe.session.user);
+              frm.set_value("gmm_date", frappe.datetime.nowdate());
+  
+
+      }
+      
+  },
+
 });
 
-frappe.ui.form.on("Purchase Order", "validate", function(frm) {    
-      if(frm.doc.workflow_state === 'Pending For FC Approval' && frappe.user.has_role(['PO Manager'])){
-      frm.doc.po_manager_name = frappe.session.user
-  frm.doc.pom_date = frappe.datetime.nowdate()
-    } 
-      else if(frm.doc.workflow_state === 'Pending For GM Approval' && frappe.user.has_role(['FC'])){
-      frm.doc.fc_name = frappe.session.user
-frm.doc.fcm_date = frappe.datetime.nowdate()
-    }
-    
-    else if(frm.doc.workflow_state === 'Approved By GM' && frappe.user.has_role(['GM'])){
-      frm.doc.gm_approver_name = frappe.session.user
-frm.doc.gmm_date = frappe.datetime.nowdate()
-    }
-});
+
+
+
