1129420c5843c5c39a5d278add91382731139681
shivansh0608 <shivanshkashyap590@gmail.com>
2024-09-15 12:48:10 +0300

    fixtures corrected

--- amcc_app/hooks.py
+++ amcc_app/hooks.py
@@ -202,5 +202,21 @@ user_data_fields = [
 # ]
 
 fixtures = [
-    "Purchase Order",
+{"dt": "Custom Field", 
+        "filters": [
+            [
+                "name", "in", {
+                    "Purchase Order-approval_section",
+                    "Purchase Order-po_manager_name",
+                    "Purchase Order-pom_date",
+                    "Purchase Order-column_break_duvjp",
+                    "Purchase Order-fc_name",
+                    "Purchase Order-fcm_date",
+                    "Purchase Order-gm_approver_name",
+                    "Purchase Order-gmm_date",
+                    
+                }
+            ]
+        ]
+    }
 ]
